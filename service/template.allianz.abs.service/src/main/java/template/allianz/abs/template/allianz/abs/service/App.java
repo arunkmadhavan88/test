9d97da934f57b6e955e0717e92c4582b1fb09014
arun <arunkmadhavan88@gmail.com>
2019-04-12 09:08:32 +0530

    AKM::initial workspace

---
+++ service/template.allianz.abs.service/src/main/java/template/allianz/abs/template/allianz/abs/service/App.java
@@ -0,0 +1,13 @@
+package template.allianz.abs.template.allianz.abs.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
